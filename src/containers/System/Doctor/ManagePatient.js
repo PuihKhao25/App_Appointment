0f6166efcb9901006ef9a1e0e30c8d0c5f3f6e01
PuihKhao25 <pkhao1999@gmail.com>
2022-06-02 16:48:14 +0700

    display patient list management

--- src/containers/System/Doctor/ManagePatient.js
+++ src/containers/System/Doctor/ManagePatient.js
@@ -3,19 +3,41 @@ import { connect } from "react-redux";
 import { FormattedMessage } from 'react-intl';
 import './ManagePatient.scss';
 import DatePicker from '../../../components/Input/DatePicker';
+import { getAllPatientForDoctor } from '../../../services/userService'
+import moment from 'moment';
 
 class ManagePatient extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            currentDate: new Date()
+            currentDate: moment(new Date()).startOf('day').valueOf(),
+            dataPatent: []
         }
     }
 
     async componentDidMount() {
+        let { user } = this.props;
+        let { currentDate } = this.state;
+
+        let formateDate = new Date(currentDate).getTime();
+
+        this.getDataPatient(user, formateDate)
 
     }
+
+    getDataPatient = async (user, formateDate) => {
+        let res = await getAllPatientForDoctor({
+            doctorId: user.id,
+            date: formateDate
+        })
+        if (res && res.errCode === 0) {
+            this.setState({
+                dataPatent: res.data
+            })
+        }
+    }
+
     async componentDidUpdate(prevProps, prevState, snapshot) {
         if (this.props.language !== prevProps.language) {
 
@@ -24,10 +46,24 @@ class ManagePatient extends Component {
     handleOnchangeDatePicker = (date) => {
         this.setState({
             currentDate: date[0]
+        }, () => {
+            let { user } = this.props;
+            let { currentDate } = this.state;
+
+            let formateDate = new Date(currentDate).getTime();
+            this.getDataPatient(user, formateDate)
         })
+    }
+    handleBtnConfirm =()=>{
+        alert('confirm')
+    }
+    handleBtnRemedy = ()=>{
+        alert('remedy')
     }
 
     render() {
+        console.log('dddddasaaa', this.state)
+        let { dataPatent } = this.state
         return (
             <div className='manage-patient-container'>
                 <div className='m-p-title'>
@@ -37,21 +73,56 @@ class ManagePatient extends Component {
                     <div className='col-6 form-group'>
                         <label>Chọn ngày tháng</label>
                         <DatePicker
-                                onChange={this.handleOnchangeDatePicker}
-                                className='form-control'
-                                value={this.state.currentDate}
-                             
-                            />
+                            onChange={this.handleOnchangeDatePicker}
+                            className='form-control'
+                            value={this.state.currentDate}
+
+                        />
                     </div>
                     <div className='col-12 table-manage-patient'>
-                        <table style={{width:'100%'}}>
-                            <tr>
-                                <th>name</th>
-                                <th>hi</th>
-                            </tr>
-                            <tr>
-                                <td>bill dfd</td>
-                            </tr>
+                        <table style={{ width: '100%' }}>
+                            <tbody>
+                                <tr>
+                                    <th>STT</th>
+                                    <th>Thời gian</th>
+                                    <th>Họ và tên</th>
+                                    <th>Địa chỉ</th>
+                                    <th>Giới tính</th>
+                                    <th>actions</th>
+                                </tr>
+                                {dataPatent && dataPatent.length > 0 ?
+                                    dataPatent.map((item, index) => {
+                                        return (
+                                            <tr key={index}>
+                                                <td>{index + 1}</td>
+                                                <td>{item.timeTypeDataPatient.valueVi}</td>
+                                                <td> {item.patientData.firstName} </td>
+                                                <td> {item.patientData.address}</td>
+                                                <td>{item.patientData.genderData.valueVi}</td>
+                                                <td>
+                                                    <button 
+                                                    className='mp-btn-confirm'
+                                                    onClick={()=>this.handleBtnConfirm()}
+                                                    >
+                                                        Xác nhận 
+                                                        </button>
+                                                    <button
+                                                     className='mp-btn-remedy'
+                                                     onClick={()=> this.handleBtnRemedy()}
+                                                     >
+                                                         Gửi hóa đơn
+                                                         </button>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                    :
+                                    <tr>
+                                        No data
+                                    </tr>
+                                }
+
+                            </tbody>
                         </table>
                     </div>
 
@@ -63,7 +134,8 @@ class ManagePatient extends Component {
 
 const mapStateToProps = state => {
     return {
-        language: state.app.language
+        language: state.app.language,
+        user: state.user.userInfo,
     };
 };
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -89,6 +89,11 @@ const getAllClinic = () => {
 const getAllDetailClinicById = (data) => {
     return axios.get(`/api/get-detail-clinic-by-id?id=${data.id}`)
 }
+
+const getAllPatientForDoctor= (data) => {
+    return axios.get(`/api/get-list-patient-for-doctor?doctorId=${data.doctorId}&date=${data.date}`)
+}
+
 export {
     handleLoginApi,
     getAllUsers,
@@ -111,5 +116,6 @@ export {
     getAllDetailSpecialtyById,
     createNewClinic,
     getAllClinic,
-    getAllDetailClinicById
+    getAllDetailClinicById,
+    getAllPatientForDoctor
 }
